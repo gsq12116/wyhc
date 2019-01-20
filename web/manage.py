c808ecebef8ce4a961b04266c9dff8dae1c919b1
Gushangquan <2546903650@qq.com>
2019-01-20 21:07:37 +0800

    web second

--- web/manage.py
+++ web/manage.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
 
 from web.app.views import blue
+from web.setting import DATABASES
+
 
 app = Flask(__name__)
 
+# 密钥配置
 app.config['SECRET_KEY'] = '123'
 app.config['JSON_AS_ASCII'] = False
 
-app.register_blueprint(blue, url_prefix='/goods')
+# 数据库配置
+default_database = DATABASES['default']
+app.config['SQLALCHEMY_DATABASE_URI'] = '{}+{}://{}:{}@{}:{}/{}'.format(default_database['DRIVER'],
+                                                                        default_database['DH'],
+                                                                        default_database['USER'],
+                                                                        default_database['PASSWORD'],
+                                                                        default_database['HOST'],
+                                                                        default_database['PORT'],
+                                                                        default_database['NAME'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# 蓝图绑定
+app.register_blueprint(blueprint=blue, url_prefix='/goods')
 
 
 @app.route('/')
---
+++ web/setting.py
@@ -0,0 +1,11 @@
+DATABASES = {
+        'default': {
+            'DRIVER': 'mysql',
+            'DH': 'pymysql',
+            'NAME': 'ihome',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': 3306
+        }
+    }
